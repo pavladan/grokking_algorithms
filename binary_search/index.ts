79cb5d21a0341eab4e98588958e3dd25b58f6d55
pavladan <fatalfenker@gmail.com>
2021-05-12 17:35:54 +0300

    [FEAT] add binary search algorithm

---
+++ binary_search/index.ts
@@ -0,0 +1,18 @@
+export default <T>(list: T[], item: T) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guess = list[mid];
+    if (guess === item) {
+      return mid;
+    }
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+};
